9eb55c1b7660b1e90202f1b7a58fe71d602df462
Westley Bonack <westley.bonack@gmail.com>
2014-06-01 09:10:51 -0500

    Modify filter by age

--- events.js
+++ events.js
@@ -19,6 +19,7 @@ function GetEventsById(ids)
           {
             arrayTest.push(userSnap);
             // console.log(userSnap.val());
+
             jQuery("#eventsDiv").append("Event: " + userSnap.val().title + "<br/>");
             ids.splice(i, 1);
           }
@@ -28,7 +29,7 @@ function GetEventsById(ids)
 
     setIds(arrayTest);
 
-    console.log(arrayTest);
+    //console.log(arrayTest);
 
     // console.log(arrayTest[0]);
     // console.log(arrayTest.length);
@@ -43,11 +44,11 @@ function GetAllIds()
   fb.once('value', function(snapshot) {
     var idArray = new Array();
     snapshot.forEach(function(userSnap) {
-      console.log(userSnap.name());
+     // console.log(userSnap.name());
       idArray.push(userSnap.name());
     });
     setIds(idArray);
-    GetEvents();
+    GetEventsById();
   });
 }
 
@@ -71,7 +72,7 @@ function setIds(newIds)
 
 // Filter by Age
 
-function FilterByAge(ids)
+function FilterByAge(ids, age)
 {
   var arrayTest = new Array();
   var fb = new Firebase("https://amber-fire-6558.firebaseio.com/data/events");
@@ -82,19 +83,24 @@ function FilterByAge(ids)
       // jQuery("#eventsDiv").append("Event: " + userSnap.val().title + "<br/>");
       if (ids == undefined)
       {
-          jQuery("#eventsDiv").append("Event: " + userSnap.val().title + "<br/>");
+          // jQuery("#eventsDiv").append("Event: " + userSnap.val().title + "<br/>");
       }
       else
       {
         ids.forEach(function(idName)
         {
-          if (userSnap.name() == idName || ids == undefined)
+          // console.log(userSnap.val());
+          //console.log(userSnap.val());
+          //console.log(userSnap.val().agerange);
+          //console.log(age);
+          if ((userSnap.name() == idName && userSnap.val().agerange == age))
           {
             arrayTest.push(userSnap);
-            // console.log(userSnap.val());
+            console.log(userSnap.val());
             jQuery("#eventsDiv").append("Event: " + userSnap.val().title + "<br/>");
             ids.splice(i, 1);
           }
+          i++;
         });
       }
     });
@@ -110,6 +116,8 @@ function FilterByAge(ids)
 }
 
 setIds(new Array("-JOII_RViT5dV1rmwvuY", "-JOIJTxJyZKBGNFk1JD5"));
+
+FilterByAge(new Array("-JOII_RViT5dV1rmwvuY", "-JOIJTxJyZKBGNFk1JD5", "-JOMdblAVMQVEBzJUtr-"), "11-14");
 
 console.log("-----");
 
