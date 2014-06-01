b4441f3b97af35c185cdc8da111e2a4d3ef85ed2
Dan <dbunked@gmail.com>
2014-06-01 01:51:33 -0500

    modified admin.html

--- js/login.js
+++ js/login.js
@@ -1,6 +1,8 @@
 function login(){
 		var fb = new Firebase('https://amber-fire-6558.firebaseio.com');
-		var auth = new FirebaseSimpleLogin(fb, function(error, user) {
+		var auth = new FirebaseSimpleLogin(fb, check);
+
+		function check (error, user) {
 
 		  if (error) {
 		    // an error occurred while attempting login
@@ -17,12 +19,14 @@ function login(){
 		  	  email: $('#inputEmail3').val(),
 		  	  password: $('#inputPassword3').val()
 		  	});
-		  	console.log("User: ", usr);
+		  	
+
+		  	console.log("User: ", usr._id);
 		  	if(!usr)
 		  		alert("Incorrect email address or password.");
 		    return;
 		  }
-		});
+		}
 	
 }
 
