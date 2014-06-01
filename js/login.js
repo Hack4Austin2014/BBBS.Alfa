5270934732f1d879104499ae95e8e4c332826986
Dan <dbunked@gmail.com>
2014-06-01 05:18:47 -0500

    login stuff

--- js/login.js
+++ js/login.js
@@ -1,43 +1,60 @@
-function login(){
-		var fb = new Firebase('https://amber-fire-6558.firebaseio.com');
-		var auth = new FirebaseSimpleLogin(fb, check);
 
-		function check (error, user) {
+var fb = new Firebase('https://amber-fire-6558.firebaseio.com');
+var auth = new FirebaseSimpleLogin(fb, function (error, user){
+	loadContent();
+	// if(error){
+	// 	console.log("There was an error!!!");
+	// 	console.log(error);
+	// }
+	// else if(user){
+	// 	loadContent();
+	// }
+	// else{
+	// 	console.log("no user logged in");
+	// 	displayLoginForm();
+	// }
 
-		  if (error) {
-		    // an error occurred while attempting login
-		    console.log(error);
-		    alert("An error occurred while trying to sign in.");
-		    return ;
-		  } else if (user) {
-		    console.log(user);
-		    loadContent();
-		    return;
-		  } else {
-		  	console.log("attempting to login with: ", $('#inputEmail3').val(), "and", $('#inputPassword3').val());
+});
 
-		  	var usr = auth.login('password', {
-		  	  email: $('#inputEmail3').val(),
-		  	  password: $('#inputPassword3').val()
-		  	});
+function loadContent (){
+	$.get('inputEvent.html', null, function (mydata, textStatus, jqXHR){
+		$("#main-content").html(mydata);
+	});
+}
 
-		  	if(!usr._id){
-		  		alert("Incorrect email address or password.");
-		  		console.log("couldn't log in")
-		  	}
-		  	else{
-		  		console.log("login successful!")
-		  		loadContent();
-		  	}
-		    return;
-		  }
-		}
+function displayLoginForm(){
+	var formHTML = '<form class="form-horizontal" role="form" onsubmit="window.mylogin()">\
+				    <div class="form-group">\
+				      <label for="inputEmail3" class="col-sm-2 control-label">Email</label>\
+				      <div class="col-sm-5">\
+				        <input type="email" class="form-control" id="inputEmail3" placeholder="Email">\
+				      </div>\
+				    </div>\
+				    <div class="form-group">\
+				      <label for="inputPassword3" class="col-sm-2 control-label">Password</label>\
+				      <div class="col-sm-5">\
+				        <input type="password" class="form-control" id="inputPassword3" placeholder="Password">\
+				      </div>\
+				    </div>\
+				    <div class="form-group">\
+				      <div class="col-sm-offset-2 col-sm-10">\
+				        <button id="submitButton" type="submit" class="btn btn-default">Sign in</button>\
+				      </div>\
+				    </div>\
+				  </form>';
 
-		function loadContent (){
-			console.log("loading content...");
+	$( "#main-content" ).html(formHTML);			  
 
-			//$( "#main-content" ).html( "HELLO!" );
-		}
-	
+}
+
+var mylogin = function (){
+	console.dir(window.auth);
+   	var user_email = $('#inputEmail3').val();
+   	var user_password = $('#inputPassword3').val();
+console.log(user_email, user_password);
+   	window.auth.login('password', {
+  	  email: user_email,
+  	  password: user_password
+  	});
 }
 
