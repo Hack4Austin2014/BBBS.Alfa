470f4cac81eea8ec4042f946a17ce40af72d8880
Dan <dbunked@gmail.com>
2014-06-01 01:19:42 -0500

    modified admin.html

--- js/login.js
+++ js/login.js
@@ -1,9 +1,4 @@
 function login(){
-	
-
-	$.getScript('https://cdn.firebase.com/js/simple-login/1.4.1/firebase-simple-login.js', function (response, status){
-		console.log(status);
-		alert("running script");
 		var fb = new Firebase('https://amber-fire-6558.firebaseio.com');
 		var auth = new FirebaseSimpleLogin(fb, function(error, user) {
 		  if (error) {
@@ -26,6 +21,6 @@ function login(){
 		    return;
 		  }
 		});
-	});
+	
 }
 
