279370518d69f8d80747572bf1915be33738424a
Jose Manriquez <josejlm2@hotmail.com>
2014-06-01 04:28:01 -0500

    js adding to radio js

--- js/flatui-radio.js
+++ js/flatui-radio.js
@@ -137,3 +137,457 @@
   });
 
 }(window.jQuery);
+
+
+(function( window, $, undefined ) {
+	
+	/*
+	* smartresize: debounced resize event for jQuery
+	*
+	* latest version and complete README available on Github:
+	* https://github.com/louisremi/jquery.smartresize.js
+	*
+	* Copyright 2011 @louis_remi
+	* Licensed under the MIT license.
+	*/
+
+	var $event = $.event, resizeTimeout;
+
+	$event.special.smartresize 	= {
+		setup: function() {
+			$(this).bind( "resize", $event.special.smartresize.handler );
+		},
+		teardown: function() {
+			$(this).unbind( "resize", $event.special.smartresize.handler );
+		},
+		handler: function( event, execAsap ) {
+			// Save the context
+			var context = this,
+				args 	= arguments;
+
+			// set correct event type
+			event.type = "smartresize";
+
+			if ( resizeTimeout ) { clearTimeout( resizeTimeout ); }
+			resizeTimeout = setTimeout(function() {
+				jQuery.event.handle.apply( context, args );
+			}, execAsap === "execAsap"? 0 : 100 );
+		}
+	};
+
+	$.fn.smartresize 			= function( fn ) {
+		return fn ? this.bind( "smartresize", fn ) : this.trigger( "smartresize", ["execAsap"] );
+	};
+	
+	$.Accordion 				= function( options, element ) {
+	
+		this.$el			= $( element );
+		// list items
+		this.$items			= this.$el.children('ul').children('li');
+		// total number of items
+		this.itemsCount		= this.$items.length;
+		
+		// initialize accordion
+		this._init( options );
+		
+	};
+	
+	$.Accordion.defaults 		= {
+		// index of opened item. -1 means all are closed by default.
+		open			: -1,
+		// if set to true, only one item can be opened. Once one item is opened, any other that is opened will be closed first
+		oneOpenedItem	: false,
+		// speed of the open / close item animation
+		speed			: 600,
+		// easing of the open / close item animation
+		easing			: 'easeInOutExpo',
+		// speed of the scroll to action animation
+		scrollSpeed		: 900,
+		// easing of the scroll to action animation
+		scrollEasing	: 'easeInOutExpo'
+    };
+	
+	$.Accordion.prototype 		= {
+		_init 				: function( options ) {
+			
+			this.options 		= $.extend( true, {}, $.Accordion.defaults, options );
+			
+			// validate options
+			this._validate();
+			
+			// current is the index of the opened item
+			this.current		= this.options.open;
+			
+			// hide the contents so we can fade it in afterwards
+			this.$items.find('div.st-content').hide();
+			
+			// save original height and top of each item	
+			this._saveDimValues();
+			
+			// if we want a default opened item...
+			if( this.current != -1 )
+				this._toggleItem( this.$items.eq( this.current ) );
+			
+			// initialize the events
+			this._initEvents();
+			
+		},
+		_saveDimValues		: function() {
+		
+			this.$items.each( function() {
+				
+				var $item		= $(this);
+				
+				$item.data({
+					originalHeight 	: $item.find('a:first').height(),
+					offsetTop		: $item.offset().top
+				});
+				
+			});
+			
+		},
+		// validate options
+		_validate			: function() {
+		
+			// open must be between -1 and total number of items, otherwise we set it to -1
+			if( this.options.open < -1 || this.options.open > this.itemsCount - 1 )
+				this.options.open = -1;
+	 	
+		},
+		_initEvents			: function() {
+			
+			var instance	= this;
+			
+			// open / close item
+			this.$items.find('a:first').bind('click.accordion', function( event ) {
+				
+				var $item			= $(this).parent();
+				
+				// close any opened item if oneOpenedItem is true
+				if( instance.options.oneOpenedItem && instance._isOpened() && instance.current!== $item.index() ) {
+					
+					instance._toggleItem( instance.$items.eq( instance.current ) );
+				
+				}
+				
+				// open / close item
+				instance._toggleItem( $item );
+				
+				return false;
+			
+			});
+			
+			$(window).bind('smartresize.accordion', function( event ) {
+				
+				// reset orinal item values
+				instance._saveDimValues();
+			
+				// reset the content's height of any item that is currently opened
+				instance.$el.find('li.st-open').each( function() {
+					
+					var $this	= $(this);
+					$this.css( 'height', $this.data( 'originalHeight' ) + $this.find('div.st-content').outerHeight( true ) );
+				
+				});
+				
+				// scroll to current
+				if( instance._isOpened() )
+				instance._scroll();
+				
+			});
+			
+		},
+		// checks if there is any opened item
+		_isOpened			: function() {
+		
+			return ( this.$el.find('li.st-open').length > 0 );
+		
+		},
+		// open / close item
+		_toggleItem			: function( $item ) {
+			
+			var $content = $item.find('div.st-content');
+			
+			( $item.hasClass( 'st-open' ) ) 
+					
+				? ( this.current = -1, $content.stop(true, true).fadeOut( this.options.speed ), $item.removeClass( 'st-open' ).stop().animate({
+					height	: $item.data( 'originalHeight' )
+				}, this.options.speed, this.options.easing ) )
+				
+				: ( this.current = $item.index(), $content.stop(true, true).fadeIn( this.options.speed ), $item.addClass( 'st-open' ).stop().animate({
+					height	: $item.data( 'originalHeight' ) + $content.outerHeight( true )
+				}, this.options.speed, this.options.easing ), this._scroll( this ) )
+		
+		},
+		// scrolls to current item or last opened item if current is -1
+		_scroll				: function( instance ) {
+			
+			var instance	= instance || this, current;
+			
+			( instance.current !== -1 ) ? current = instance.current : current = instance.$el.find('li.st-open:last').index();
+			
+			$('html, body').stop().animate({
+				scrollTop	: ( instance.options.oneOpenedItem ) ? instance.$items.eq( current ).data( 'offsetTop' ) : instance.$items.eq( current ).offset().top
+			}, instance.options.scrollSpeed, instance.options.scrollEasing );
+		
+		}
+	};
+	
+	var logError 				= function( message ) {
+		
+		if ( this.console ) {
+			
+			console.error( message );
+			
+		}
+		
+	};
+	
+	$.fn.accordion 				= function( options ) {
+	
+		if ( typeof options === 'string' ) {
+		
+			var args = Array.prototype.slice.call( arguments, 1 );
+
+			this.each(function() {
+			
+				var instance = $.data( this, 'accordion' );
+				
+				if ( !instance ) {
+					logError( "cannot call methods on accordion prior to initialization; " +
+					"attempted to call method '" + options + "'" );
+					return;
+				}
+				
+				if ( !$.isFunction( instance[options] ) || options.charAt(0) === "_" ) {
+					logError( "no such method '" + options + "' for accordion instance" );
+					return;
+				}
+				
+				instance[ options ].apply( instance, args );
+			
+			});
+		
+		} 
+		else {
+		
+			this.each(function() {
+				var instance = $.data( this, 'accordion' );
+				if ( !instance ) {
+					$.data( this, 'accordion', new $.Accordion( options, this ) );
+				}
+			});
+		
+		}
+		
+		return this;
+		
+	};
+	
+})( window, jQuery );
+
+/*
+ * jQuery Easing v1.3 - http://gsgd.co.uk/sandbox/jquery/easing/
+ *
+ * Uses the built in easing capabilities added In jQuery 1.1
+ * to offer multiple easing options
+ *
+ * TERMS OF USE - jQuery Easing
+ * 
+ * Open source under the BSD License. 
+ * 
+ * Copyright © 2008 George McGinley Smith
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, 
+ * are permitted provided that the following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice, this list of 
+ * conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list 
+ * of conditions and the following disclaimer in the documentation and/or other materials 
+ * provided with the distribution.
+ * 
+ * Neither the name of the author nor the names of contributors may be used to endorse 
+ * or promote products derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ *  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED 
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
+ * OF THE POSSIBILITY OF SUCH DAMAGE. 
+ *
+*/
+
+// t: current time, b: begInnIng value, c: change In value, d: duration
+jQuery.easing['jswing'] = jQuery.easing['swing'];
+
+jQuery.extend( jQuery.easing,
+{
+	def: 'easeOutQuad',
+	swing: function (x, t, b, c, d) {
+		//alert(jQuery.easing.default);
+		return jQuery.easing[jQuery.easing.def](x, t, b, c, d);
+	},
+	easeInQuad: function (x, t, b, c, d) {
+		return c*(t/=d)*t + b;
+	},
+	easeOutQuad: function (x, t, b, c, d) {
+		return -c *(t/=d)*(t-2) + b;
+	},
+	easeInOutQuad: function (x, t, b, c, d) {
+		if ((t/=d/2) < 1) return c/2*t*t + b;
+		return -c/2 * ((--t)*(t-2) - 1) + b;
+	},
+	easeInCubic: function (x, t, b, c, d) {
+		return c*(t/=d)*t*t + b;
+	},
+	easeOutCubic: function (x, t, b, c, d) {
+		return c*((t=t/d-1)*t*t + 1) + b;
+	},
+	easeInOutCubic: function (x, t, b, c, d) {
+		if ((t/=d/2) < 1) return c/2*t*t*t + b;
+		return c/2*((t-=2)*t*t + 2) + b;
+	},
+	easeInQuart: function (x, t, b, c, d) {
+		return c*(t/=d)*t*t*t + b;
+	},
+	easeOutQuart: function (x, t, b, c, d) {
+		return -c * ((t=t/d-1)*t*t*t - 1) + b;
+	},
+	easeInOutQuart: function (x, t, b, c, d) {
+		if ((t/=d/2) < 1) return c/2*t*t*t*t + b;
+		return -c/2 * ((t-=2)*t*t*t - 2) + b;
+	},
+	easeInQuint: function (x, t, b, c, d) {
+		return c*(t/=d)*t*t*t*t + b;
+	},
+	easeOutQuint: function (x, t, b, c, d) {
+		return c*((t=t/d-1)*t*t*t*t + 1) + b;
+	},
+	easeInOutQuint: function (x, t, b, c, d) {
+		if ((t/=d/2) < 1) return c/2*t*t*t*t*t + b;
+		return c/2*((t-=2)*t*t*t*t + 2) + b;
+	},
+	easeInSine: function (x, t, b, c, d) {
+		return -c * Math.cos(t/d * (Math.PI/2)) + c + b;
+	},
+	easeOutSine: function (x, t, b, c, d) {
+		return c * Math.sin(t/d * (Math.PI/2)) + b;
+	},
+	easeInOutSine: function (x, t, b, c, d) {
+		return -c/2 * (Math.cos(Math.PI*t/d) - 1) + b;
+	},
+	easeInExpo: function (x, t, b, c, d) {
+		return (t==0) ? b : c * Math.pow(2, 10 * (t/d - 1)) + b;
+	},
+	easeOutExpo: function (x, t, b, c, d) {
+		return (t==d) ? b+c : c * (-Math.pow(2, -10 * t/d) + 1) + b;
+	},
+	easeInOutExpo: function (x, t, b, c, d) {
+		if (t==0) return b;
+		if (t==d) return b+c;
+		if ((t/=d/2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b;
+		return c/2 * (-Math.pow(2, -10 * --t) + 2) + b;
+	},
+	easeInCirc: function (x, t, b, c, d) {
+		return -c * (Math.sqrt(1 - (t/=d)*t) - 1) + b;
+	},
+	easeOutCirc: function (x, t, b, c, d) {
+		return c * Math.sqrt(1 - (t=t/d-1)*t) + b;
+	},
+	easeInOutCirc: function (x, t, b, c, d) {
+		if ((t/=d/2) < 1) return -c/2 * (Math.sqrt(1 - t*t) - 1) + b;
+		return c/2 * (Math.sqrt(1 - (t-=2)*t) + 1) + b;
+	},
+	easeInElastic: function (x, t, b, c, d) {
+		var s=1.70158;var p=0;var a=c;
+		if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
+		if (a < Math.abs(c)) { a=c; var s=p/4; }
+		else var s = p/(2*Math.PI) * Math.asin (c/a);
+		return -(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
+	},
+	easeOutElastic: function (x, t, b, c, d) {
+		var s=1.70158;var p=0;var a=c;
+		if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
+		if (a < Math.abs(c)) { a=c; var s=p/4; }
+		else var s = p/(2*Math.PI) * Math.asin (c/a);
+		return a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*(2*Math.PI)/p ) + c + b;
+	},
+	easeInOutElastic: function (x, t, b, c, d) {
+		var s=1.70158;var p=0;var a=c;
+		if (t==0) return b;  if ((t/=d/2)==2) return b+c;  if (!p) p=d*(.3*1.5);
+		if (a < Math.abs(c)) { a=c; var s=p/4; }
+		else var s = p/(2*Math.PI) * Math.asin (c/a);
+		if (t < 1) return -.5*(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
+		return a*Math.pow(2,-10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )*.5 + c + b;
+	},
+	easeInBack: function (x, t, b, c, d, s) {
+		if (s == undefined) s = 1.70158;
+		return c*(t/=d)*t*((s+1)*t - s) + b;
+	},
+	easeOutBack: function (x, t, b, c, d, s) {
+		if (s == undefined) s = 1.70158;
+		return c*((t=t/d-1)*t*((s+1)*t + s) + 1) + b;
+	},
+	easeInOutBack: function (x, t, b, c, d, s) {
+		if (s == undefined) s = 1.70158; 
+		if ((t/=d/2) < 1) return c/2*(t*t*(((s*=(1.525))+1)*t - s)) + b;
+		return c/2*((t-=2)*t*(((s*=(1.525))+1)*t + s) + 2) + b;
+	},
+	easeInBounce: function (x, t, b, c, d) {
+		return c - jQuery.easing.easeOutBounce (x, d-t, 0, c, d) + b;
+	},
+	easeOutBounce: function (x, t, b, c, d) {
+		if ((t/=d) < (1/2.75)) {
+			return c*(7.5625*t*t) + b;
+		} else if (t < (2/2.75)) {
+			return c*(7.5625*(t-=(1.5/2.75))*t + .75) + b;
+		} else if (t < (2.5/2.75)) {
+			return c*(7.5625*(t-=(2.25/2.75))*t + .9375) + b;
+		} else {
+			return c*(7.5625*(t-=(2.625/2.75))*t + .984375) + b;
+		}
+	},
+	easeInOutBounce: function (x, t, b, c, d) {
+		if (t < d/2) return jQuery.easing.easeInBounce (x, t*2, 0, c, d) * .5 + b;
+		return jQuery.easing.easeOutBounce (x, t*2-d, 0, c, d) * .5 + c*.5 + b;
+	}
+});
+
+/*
+ *
+ * TERMS OF USE - EASING EQUATIONS
+ * 
+ * Open source under the BSD License. 
+ * 
+ * Copyright © 2001 Robert Penner
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, 
+ * are permitted provided that the following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice, this list of 
+ * conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list 
+ * of conditions and the following disclaimer in the documentation and/or other materials 
+ * provided with the distribution.
+ * 
+ * Neither the name of the author nor the names of contributors may be used to endorse 
+ * or promote products derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ *  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED 
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
+ * OF THE POSSIBILITY OF SUCH DAMAGE. 
+ *
+ */
